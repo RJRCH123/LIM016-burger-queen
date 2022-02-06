5f358ac1a2d7368780beb090f69490a2308e6fc3
Rosamaria Rodriguez <rous9716@gmail.com>
2022-02-06 05:45:10 -0500

    Fix: Agregar index de container

---
+++ src/componentes/bienvenida/container/index.js
@@ -0,0 +1,13 @@
+import AboutUs from './AboutUs/AboutUs';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Laurels from './Laurels/Laurels';
+import SpecialMenu from './Menu/SpecialMenu';
+
+export {
+  AboutUs,
+  Footer,
+  Header,
+  Laurels,
+  SpecialMenu,
+};
