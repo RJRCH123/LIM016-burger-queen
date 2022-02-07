8fa878492bf2eddab56e12d02db1d283e8a7c865
Marielena <maizaga01@gmail.com>
2022-02-06 20:58:35 -0500

    build: vista de historialempleado

--- src/componentes/pages/historialEmpleados.js
+++ src/componentes/pages/historialEmpleados.js
@@ -3,7 +3,8 @@ import './historialEmpleados.scss';
 
 const HistorialEmpleados = () => {
     return  ( 
-        <div>
+        <div className='Historial'>
+            {/* encabezado */}
             <div className='datosEmpresa'>
                 <h2>HISTORIAL DE EMPLEADOS</h2>
                 <div className='descripcion'>
@@ -15,6 +16,77 @@ const HistorialEmpleados = () => {
                     <p>FECHA: 20 / 04 / 21</p>
                 </div>                
             </div>
+            {/* tabla de empleados */}
+            <div className='tablaEmpleados'>               
+                <div className='leyenda'>
+                    <div className='tituloTable'>
+                        <table>
+                            <tr>
+                                <th>N°</th>
+                                <th>DATOS PERSONALES</th>
+                                <th>DATOS OTORGADOS</th>
+                            </tr>
+                        </table>
+                        <div className='vacio'></div>
+                    </div>                   
+                    <div className='tablaIndividual'>
+                        <table>
+                            <tr>
+                                <th>1</th>
+                                <td>Data</td>  
+                                <td>Data</td>                              
+                            </tr>
+                        </table>
+                        <div className='botonesCRUD'>
+                            <button type="onclick" className='botonEditar'>Editar</button>
+                            <button type="onclick" className='botonEliminar'>Eliminar</button>
+                        </div>
+                    </div>
+                    <div className='tablaIndividual'>
+                        <table>
+                            <tr>
+                                <th>2</th>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                        </table>
+                        <div className='botonesCRUD'>
+                            <button type="onclick" className='botonEditar'>Editar</button>
+                            <button type="onclick" className='botonEliminar'>Eliminar</button>
+                        </div>
+                    </div> 
+                    <div className='tablaIndividual'>
+                        <table>
+                            <tr>
+                                <th>3</th>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                        </table>
+                        <div className='botonesCRUD'>
+                            <button type="onclick" className='botonEditar'>Editar</button>
+                            <button type="onclick" className='botonEliminar'>Eliminar</button>
+                        </div>
+                    </div> 
+                    <div className='tablaIndividual'>
+                        <table>
+                            <tr>
+                                <th>4</th>
+                                <td>Data</td>
+                                <td>Data</td>
+                            </tr>
+                        </table>
+                        <div className='botonesCRUD'>
+                            <button type="onclick" className='botonEditar'>Editar</button>
+                            <button type="onclick" className='botonEliminar'>Eliminar</button>
+                        </div>
+                    </div>                                             
+                </div>               
+            </div>
+            {/* sección del boton descarga */}
+            <div className='secBoton'>
+                <button type="submit">Descargar PDF</button>
+            </div>            
         </div>
     )
 } 
