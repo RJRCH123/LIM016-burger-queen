f4c328fe3b697cf3744d9eccd470a7cb5c269667
Rosamaria Rodriguez <rous9716@gmail.com>
2022-02-11 18:56:35 -0500

    Fix: Add counter for product card

--- src/componentes/pages/realizarPedido/components/btnsCancelarYConfirmar/btnsCancelarYConfirmar.jsx
+++ src/componentes/pages/realizarPedido/components/btnsCancelarYConfirmar/btnsCancelarYConfirmar.jsx
@@ -8,7 +8,6 @@ const BtnsCancelarYConfirmar = () => {
       <button type="button" className="confirm__button">Confirmar</button>
       <button type="button" className="cancel__button">Cancelar</button>
     </section>
-    <button type="button" className="limpiar__button">Limpiar</button>
   </div>;
 };
 
--- src/componentes/pages/realizarPedido/components/productoPorPedir/productoPorPedir.jsx
+++ src/componentes/pages/realizarPedido/components/productoPorPedir/productoPorPedir.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlinePlus, AiOutlineLine } from 'react-icons/ai'
 import './productoPorPedir.scss';
 
 const ProductoPorPedir = ({data, addToCart}) => { 
 
   let {codigo,name,precio,imagen} = data;
+
+  const [ count, setCount ] = useState(0);
+
+  let plus = () => {
+    setCount(count + 1)
+  }
+  let less = () => {
+    setCount(count - 1)
+  }
 
   return <div className="DataPorProductoMenu">
     <div className="imagenProducto">
@@ -14,6 +24,11 @@ const ProductoPorPedir = ({data, addToCart}) => {
       <h1>s/. {precio}0</h1>
     </div>
     <div className="agregarYaumentar">
+      <div className="contador">
+        <button onClick={less} type="button" > <AiOutlineLine /> </button>
+        <p> {count} </p>
+        <button onClick={plus} type="button"> <AiOutlinePlus  /> </button>
+      </div>
       <button onClick={() => addToCart(codigo)} type="button" className="Agregar__button">Agregar</button>
     </div>
 </div>;
