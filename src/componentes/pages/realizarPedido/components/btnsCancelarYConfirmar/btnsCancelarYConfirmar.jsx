246b5a9a2d9a77cc55000772adbbb7ff78d8bea8
Rosamaria Rodriguez <rous9716@gmail.com>
2022-02-14 05:04:02 -0500

    Create: Modals confirmation for 'Cancelar y confirmar pedido'

--- src/componentes/pages/realizarPedido/components/btnsCancelarYConfirmar/btnsCancelarYConfirmar.jsx
+++ src/componentes/pages/realizarPedido/components/btnsCancelarYConfirmar/btnsCancelarYConfirmar.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 import './btnsCancelarYConfirmar.scss';
 
 const BtnsCancelarYConfirmar = () => {
 
+  
+  const modalConfirmarOrden = () => {
+    Swal.fire({
+      title: 'Detalle del Pedido',
+      text: '¿Está seguro que desea enviar el pedido a cocina?',
+      showCancelButton: true,
+      confirmButtonColor: '#57a057',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title:'¡Pedido Confirmado!',
+          text: 'El pedido fue enviado a cocina con éxito',
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 1500}
+        )
+      }
+    })
+  }
+
+  const modalCancelarOrden = () => {
+    Swal.fire({
+      text: '¿Está seguro que desea cancelar el pedido?',
+      showCancelButton: true,
+      confirmButtonColor: '#57a057',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Confirmar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title:'¡Pedido Cancelado!',
+          text: 'El pedido fue cancelado con éxito',
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 1500}
+        )
+      }
+    })
+  }
+
+
   return <div>
     <section className="btnsContainer gridResponsiveBtns">
-      <button type="button" className="confirm__button">Confirmar</button>
-      <button type="button" className="cancel__button">Cancelar</button>
+      <button type="button" onClick={() => modalConfirmarOrden()}  className="confirm__button">Confirmar</button>
+      <button type="button" onClick={() => modalCancelarOrden()}  className="cancel__button">Cancelar</button>
     </section>
   </div>;
 };
