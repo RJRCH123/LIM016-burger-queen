7b39de43f93679dbebae413ddf6d54b3487cc0a8
Marielena <maizaga01@gmail.com>
2022-02-14 18:15:03 -0500

    formulario de registro con creación de usuario en firestore y validación de los input

--- src/componentes/pages/registroUsuario/AppRegistro.jsx
+++ src/componentes/pages/registroUsuario/AppRegistro.jsx
@@ -1,4 +1,7 @@
-import React, {useState} from 'react';
+import React from 'react';
+import { useState } from 'react';
+import { db } from '../../../firebase/firebase-config'
+import { addDoc, collection } from 'firebase/firestore'; 
 import {Formulario, ContenedorBotonCentrado, Boton, MensajeExito, MensajeError} from './elementos';
 import Input from './input';
 
@@ -7,7 +10,7 @@ const AppRegistro = () => {
     const [usuario, cambiarUsuario] = useState({campo: '', valido: null});
 	const [nombre, cambiarNombre] = useState({campo: '', valido: null});
     const [apellido, cambiarApellido] = useState({campo: '', valido: null});
-    const [edad, cambiarEdad] = useState({campo: '', valido: null});
+	const [codigo, cambiarCodigo] = useState({campo: '', valido: null});
 	const [contraseña, cambiarContraseña] = useState({campo: '', valido: null});
 	const [contraseña2, cambiarContraseña2] = useState({campo: '', valido: null});
 	const [correo, cambiarCorreo] = useState({campo: '', valido: null});
@@ -26,7 +29,7 @@ const AppRegistro = () => {
 		contraseña: /^.{4,12}$/, // 4 a 12 digitos.
 		correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 		celular: /^\d{7,9}$/, // 7 a 9 numeros,
-        edad: /^\d{1,2}$/, // 2 numeros.
+        codigo: /^[a-zA-Z0-9]{4,6}$/, // Letras, numeros.
         dni: /^\d{6,8}$/, // 6 a 8 numeros,
         cargo: /^[a-zA-ÿ\s]{1,40}$/, // Letras y espacios.
 	}
@@ -47,6 +50,23 @@ const AppRegistro = () => {
 		}
 	}
 
+	// se establece la comunicación con db de firebase para crear la colección con los datos del usuario 
+
+    const userCollectionRef = collection(db, 'usuarios')
+	const createUser = async () => {
+        await addDoc(userCollectionRef, {
+			usuario: usuario.campo,
+			nombres: nombre.campo, 
+			apellidos: apellido.campo,
+			cargo: cargo.campo,
+			correo: correo.campo,
+			dni: dni.campo,
+			celular: celular.campo,
+			contraseña: contraseña.campo,
+			codigo: codigo.campo
+		})
+    }
+
     // se define la estructura del formulario 
 
     const onSubmit = (e) => {
@@ -56,7 +76,7 @@ const AppRegistro = () => {
 			usuario.valido === 'true' &&
 			nombre.valido === 'true' &&
             apellido.valido === 'true' &&
-            edad.valido === 'true' &&
+            codigo.valido === 'true' &&
 			contraseña.valido === 'true' &&
 			contraseña2.valido === 'true' &&
 			correo.valido === 'true' &&
@@ -68,7 +88,7 @@ const AppRegistro = () => {
 			cambiarUsuario({campo: '', valido: null});
 			cambiarNombre({campo: '', valido: null});
             cambiarApellido({campo: '', valido: null});
-            cambiarEdad({campo: '', valido: null});
+            cambiarCodigo({campo: '', valido: null});
 			cambiarContraseña({campo: '', valido: null});
 			cambiarContraseña2({campo: '', valido: null});
 			cambiarCorreo({campo: '', valido: null});
@@ -118,14 +138,14 @@ const AppRegistro = () => {
 					expresionRegular={expresiones.apellido}
 				/>
                 <Input
-					estado={edad}
-					cambiarEstado={cambiarEdad}
+					estado={codigo}
+					cambiarEstado={cambiarCodigo}
 					tipo="text"
-					label="Edad"
-					placeholder="Edad"
-					name="edad"
-					leyendaError="LaEDAD solo puede contener números"
-					expresionRegular={expresiones.edad}
+					label="Codigo"
+					placeholder="codigo"
+					name="codigo"
+					leyendaError="El codigo debe tener la primera letra de su cargo y el número de usuario Ej: M001"
+					expresionRegular={expresiones.codigo}
 				/>
 				<Input
 					estado={contraseña}
@@ -198,7 +218,7 @@ const AppRegistro = () => {
                 }
 
 				<ContenedorBotonCentrado>
-					<Boton type="submit">Registrar Personal</Boton>
+					<Boton type="submit" onClick={createUser}>Registrar Personal</Boton>
 					{formularioValido === true && 
                         <MensajeExito>
                             <p>
