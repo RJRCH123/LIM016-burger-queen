07b4df68046bc824ea9296d6f1f7c1e43d46be3f
Rosamaria Rodriguez <rous9716@gmail.com>
2022-02-15 18:16:17 -0500

    Fix: Add time and date component for historials

--- src/componentes/utils/fecha/fecha.jsx
+++ src/componentes/utils/fecha/fecha.jsx
@@ -1,10 +1,24 @@
 import './fecha.scss';
 
 const Fecha = () => {
+  var mydate=new Date() 
+  var year=mydate.getYear() 
+  if (year < 1000) 
+  year+=1900 
+  var day=mydate.getDay() 
+  var month=mydate.getMonth() 
+  var daym=mydate.getDate() 
+  if (daym<10) 
+  daym="0"+daym 
+  /*var dayarray = new Array("Domingo,", "Lunes,", "Martes,", "Miércoles,", "Jueves,", "Viernes,", "Sábado,")*/
+  var dayarray = new Array("Dom,", "Lun,", "Mar,", "Mie,", "Jue,", "Vie,", "Sab,")
+  /*var montharray = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")*/
+  var montharray = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+
     return  ( 
         <div>
-           <p>Fecha 09/02/2022</p>
-           <p>Hora  9:22 AM</p>
+           <p>Fecha: {dayarray[day]} {daym} de {montharray[month]} de {year} </p>
+           <p>Hora: {mydate.toLocaleTimeString()} </p>
         </div>
     )
 } 
