236c228ee5ad455aeb5a851fbd5055d9b5db9361
Marielena <maizaga01@gmail.com>
2022-02-15 02:18:50 -0500

    style: change in the color of hover and icons in the view historial empleado

--- src/componentes/utils/tablaEmpleados/botonesCRUD.jsx
+++ src/componentes/utils/tablaEmpleados/botonesCRUD.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './botonesCRUD.scss';
+import BtnBorrar from '../../../img/iconos/eliminar.png'
+import Editar from '../../../img/iconos/editar.png'
 
 const botones = () => {
     return  ( 
         <div className='botonesCRUD'>
-            <button type="onclick" className='botonEditar'>Editar</button>
-            <button type="onclick" className='botonEliminar'>Eliminar</button>
+            <button type="onclick" className='botonEditar'><img src={Editar} alt= "Editar"/></button>
+            <button type="onclick" className='botonEliminar'><img src={BtnBorrar} alt= "BtnBorrar"/></button>
         </div>        
     )
 } 
