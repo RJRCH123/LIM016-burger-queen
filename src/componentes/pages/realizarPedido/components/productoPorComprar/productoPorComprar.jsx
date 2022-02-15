f0c99011461689a8921b32e6b8ed713c6ebe2619
Rosamaria Rodriguez <rous9716@gmail.com>
2022-02-15 03:00:39 -0500

    Style: Button '+' y '-'

--- src/componentes/pages/realizarPedido/components/productoPorComprar/productoPorComprar.jsx
+++ src/componentes/pages/realizarPedido/components/productoPorComprar/productoPorComprar.jsx
@@ -11,18 +11,21 @@ function ProductoPorComprar() {
     {pedido.map((item, id) => (
     <div className="dataTablaResumenPedido" key={id}>
       <div className="dataProductoRP">
-        <p> {item.name} </p>
+        <p> {item.name.toUpperCase()} </p>
       </div>
       <div className="dataUndsRP">
-        <p> {item.count} </p>
+        <button className="btnLessRP" onClick={() => { less(item.id) }} type="button" > <AiOutlineLine fontSize="1.2em"
+ color="white" 
+/> </button>
+          <p> {item.count} </p>
+        <button className="btnPlusRP" onClick={() => { plus(item.id) }} type="button" > <AiOutlinePlus fontSize="1.2em"
+ color="white" 
+ /> </button>
       </div>
       <div className="dataPrecioRP">
         <p> {item.precio} </p>
       </div>
-      <button  onClick={() => { less(item.id) }} type="button" > <AiOutlineLine /> </button>
-      <button  onClick={() => { plus(item.id) }} type="button" > <AiOutlinePlus /> </button>
-
-      <button onClick={() => eliminar(item.id)} type="button" className="dataEliminarRP">
+      <button onClick={() => eliminar(item.id)} type="button" className="dataEliminarRP btnEliminarRP">
         <img src={images.eliminar} alt="eliminar" />
       </button>
     </div>
