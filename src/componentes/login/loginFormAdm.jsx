78ff77c25ac81eb32729fae09bc5d30b5a950540
Marielena <maizaga01@gmail.com>
2022-02-21 11:14:15 -0500

    refact: revisión de función y modales para opción de olvido contraseña

--- src/componentes/login/loginFormAdm.jsx
+++ src/componentes/login/loginFormAdm.jsx
@@ -51,19 +51,17 @@ function FormAdmin() {
 		return user		
   	}
 
-  const getRol = async (userName, userRol, userMail) => {
-    const userCollectionRef = query(collection(db, "usuarios"), where("usuario", "==", userName), where("cargo", "==", userRol), where("correo", "==", userMail));
-    const dataDocs = await getDocs(userCollectionRef);
-    const user = dataDocs.docs[0].data();	
-    return user		
-    }
+	const getRol = async (userName, userRol, userMail) => {
+		const userColRef = query(collection(db, "usuarios"), where("correo", "==", userMail));
+		const dataDocs = await getDocs(userColRef);
+		const user = dataDocs.docs[0].data();	
+		console.log(user);
+		return user		
+	}
 
 	const forgotPwsd = (e) => {
 		e.preventDefault();
-		getRol(userName).then((user) => {
-			if (user.cargo){
-				if(user.cargo === "admin" ){
-          Swal.fire({
+		Swal.fire({
             title: 'Recuperación de cuenta',
             text: 'Ingresa tu correo electrónico para enviarle un enlace de recuperación',
             input: 'email',
@@ -73,62 +71,8 @@ function FormAdmin() {
             showCancelButton: true,
             confirmButtonText: 'Enviar',
             showLoaderOnConfirm: true,  
-          }).then(function (email) {
-            Swal.fire({
-              icon: 'success',
-              html: 'Your email: ' + email
-            })
-          })
-				}
-				else if(user.cargo === "mesero" | user.cargo === "cocinero"){
-					Swal.fire({
-						title: 'Opción Incorrecta',
-						text: 'Solo el administrador tiene la opción de recuperar una cuenta. Por favor, acercarse al administrador para poder recuperar su cuenta.',
-						allowOutsideClick: false,
-						stopKeydownPropagation: false,
-						showCloseButton: true,
-						closeButtonAriaLabel: 'cerrar alerta'
-					});
-				}
-			}		
-		}).catch(() => {
-        /* Swal.fire({
-				title: 'Usuario Inválido',
-				text: 'Por favor, revise que su usuario se encuentre correctamente escrito.',
-				allowOutsideClick: false,
-				stopKeydownPropagation: false,
-				showCloseButton: true,
-				closeButtonAriaLabel: 'cerrar alerta'
-			  }); */
-        Swal.fire({
-          title: 'Recuperación de cuenta',
-          text: 'Ingresa tu correo electrónico para enviarle un enlace de recuperación',
-          input: 'email',
-          inputAttributes: {
-            autocapitalize: 'off'
-          },
-          showCancelButton: true,
-          confirmButtonText: 'Enviar',
-          showLoaderOnConfirm: true,  
-        }).then((result) => {
-          if (result.isConfirmed) {
-            Swal.fire({
-              title:`¡Correo enviado a ${result.value}!`,
-              text: 'Por favor, revise su bandeja de entrada',
-              icon: 'success',
-              showConfirmButton: false,
-              timer: 2000
-            })} else {
-              Swal.fire({
-                title:`¡El correo es incorrecto!`,
-                text: `El email ${result.value} no coincide con el registrado para este usuario`,
-                icon: 'error',
-                showConfirmButton: false,
-                timer: 2000
-              })}; 
-            /* cleanInputs() */         
-        })
-		});
+        });
+		//getRol();
 	} 
 
 	return (
