21ae4e8efe89d49ff05a9525121a86ee21c457b1
Marielena <maizaga01@gmail.com>
2022-02-23 11:55:18 -0500

    refact: cambios de firebase-config para pwa offline

--- src/firebase/firebase-config.jsx
+++ src/firebase/firebase-config.jsx
@@ -16,17 +16,17 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 enableIndexedDbPersistence(db)
-  .catch((err) => {
-      if (err.code == 'failed-precondition') {
-          // Multiple tabs open, persistence can only be enabled
-          // in one tab at a a time.
-          // ...
-      } else if (err.code == 'unimplemented') {
-          // The current browser does not support all of the
-          // features required to enable persistence
-          // ...
-      }
-  });
+    .catch((err) => {
+        if (err.code == 'failed-precondition') {
+            // Multiple tabs open, persistence can only be enabled
+            // in one tab at a a time.
+            // ...
+        } else if (err.code == 'unimplemented') {
+            // The current browser does not support all of the
+            // features required to enable persistence
+            // ...
+        }
+    });
 
 export const auth = getAuth();
 export { db };
