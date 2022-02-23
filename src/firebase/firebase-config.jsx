ff8996821d5e4e405e5a99bd5b09fef05c4c6eb7
Marielena <maizaga01@gmail.com>
2022-02-23 09:42:10 -0500

    refactor: 3er intent to config pwa, we add the 'Persistence' rule in firebase-config

--- src/firebase/firebase-config.jsx
+++ src/firebase/firebase-config.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, enableIndexedDbPersistence } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
@@ -13,9 +13,23 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+enableIndexedDbPersistence(db)
+  .catch((err) => {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
 
 export const auth = getAuth();
-export const db = getFirestore(app);
+export { db };
 export const storage = getStorage(app);
 
 
