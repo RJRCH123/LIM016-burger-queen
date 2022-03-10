5af3c6c209214105236bae056d2f1a9cc4a9be09
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-10 08:38:42 -0500

    Refactor: Style in buttons in welcome

--- src/componentes/bienvenida/constants/images.jsx
+++ src/componentes/bienvenida/constants/images.jsx
@@ -1,23 +1,19 @@
 import bg from '../assets/bg.png';
 import G from '../assets/G.png';
 import menu from '../assets/menu.png';
-import overlaybg from '../assets/overlaybg.png';
-import spoon from '../assets/spoon.svg';
+import spoon from '../assets/spoon.png';
 import welcome from '../assets/welcome.png';
 import laurels from '../assets/laurels.png';
 import awards from '../assets/awards.png';
-import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
 // eslint-disable-next-line
 export default {
   bg,
   G,
   menu,
-  overlaybg,
   spoon,
   welcome,
   laurels,
-  quote,
   gericht,
   awards
 };
