535b8db217f8aee112ef108290a7ddb4eeadff04
Marielena <maizaga01@gmail.com>
2022-03-21 23:56:09 -0500

    first intent to use component from RealizarPedido

--- src/componentes/pages/registroProductos/registroProductos.jsx
+++ src/componentes/pages/registroProductos/registroProductos.jsx
@@ -1,42 +1,18 @@
 import './registroProductos.scss';
-import {
-  collection, query, where, getDocs, orderBy,
-} from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import Pedido from '../../utils/pedido/pedido';
-import { db } from '../../../firebase/firebase-config';
 
-function PedidosPreparados() {
-  const [orders, setOrders] = useState([]);
 
-  const getOrders = async () => {
-    const ordersCollectionRef = query(collection(db, 'pedidos'), orderBy('timestamp', 'asc'), where('estado', '==', 'preparado'));
-    const dataDocs = await getDocs(ordersCollectionRef);
-    const pedidos = dataDocs.docs;
-    const allOrders = [];
-    pedidos.forEach((pedido) => {
-      allOrders.push({ ...pedido.data(), id: pedido.id });
-    });
-    setOrders(allOrders);
-  };
+function RegistroProductos() {
 
-  useEffect(() => {
-    getOrders();
-  }, []);
-
+  
   return (
+        
     <div className="contenedorPedidos">
       <h2>REGISTRO DE PRODUCTOS</h2>
-      <div className="gridResponsivePP">
-        {/* {orders.map((order, index) => (
-          <div className="contenedorPedido" key={index}>
-            <Pedido key={order.id} orden={order} />
-          </div>
-        ))} */}
-
+      <div className="">
+          
       </div>
-    </div>
+    </div>   
   );
-}
 
-export default PedidosPreparados;
+}
+export default RegistroProductos;
