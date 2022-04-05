7e1a52df22cf27e0da242a7c491d83e1fb165a14
Rosamaria Rodriguez <rous9716@gmail.com>
2022-04-04 23:36:01 -0500

    Merge

--- src/componentes/pages/registroProductos/components/vistaPrevia/vistaPrevia.jsx
+++ src/componentes/pages/registroProductos/components/vistaPrevia/vistaPrevia.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./vistaPrevia.scss";
 
-function VistaPrevia({values}) {
-
+function VistaPrevia({ values }) {
   return (
     <section className="containerPreviewB">
-
       <section className="tittleInfoB">
         <h3>VISTA PREVIA</h3>
       </section>
@@ -13,34 +11,39 @@ function VistaPrevia({values}) {
       <article className="modalDescripcionB">
         <div>
           <div className="nameInfoB">
-            <h5>{values.name ?? ''}</h5>
+            <h5>{values.name ?? ""}</h5>
           </div>
           <div className="detailsInfoB">
             <p>
-              <strong> Código: </strong> {values.codigo ?? ''}
+              <strong> Código: </strong> {values.codigo ?? ""}
             </p>
             <p>
-              <strong> Categoría: </strong> {values.tipo ?? ''}
+              <strong> Categoría: </strong> {values.tipo ?? ""}
             </p>
             <p>
-              <strong> Descripción: </strong>{values.descripcion ?? ''}
+              <strong> Descripción: </strong>
+              {values.descripcion ?? ""}
             </p>
             <p>
-              <strong> Unds. por Plato: </strong>{values.undsPorPlato ?? ''}
+              <strong> Unds. por Plato: </strong>
+              {values.undsPorPlato ?? ""}
             </p>
             <br />
             <section className="precioInfoB">
-              
-              <h6>S/. {values.precio ?? ''}</h6>
+              <h6>S/. {values.precio ?? ""}</h6>
             </section>
           </div>
         </div>
-        <div className="productB">                  
-          <img                    
-            src=  {values.img !== '' ? values.img : 'https://raw.githubusercontent.com/RJRCH123/LIM016-burger-queen/main/src/componentes/pages/registroProductos/assets/addProduct.png' }
-            alt="imagen" 
-            style={{width:"100%"}}
-          />           
+        <div className="productB">
+          <img
+            src={
+              values.img !== ""
+                ? values.img
+                : "https://raw.githubusercontent.com/RJRCH123/LIM016-burger-queen/main/src/componentes/pages/registroProductos/assets/addProduct.png"
+            }
+            alt="imagen"
+            style={{ width: "100%" }}
+          />
         </div>
       </article>
     </section>
